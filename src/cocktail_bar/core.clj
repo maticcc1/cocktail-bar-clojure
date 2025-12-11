8cd0def4a89332f594c22f3e38127aa002917133
Matic Vladimir <vladimirmatic2002@gmail.com>
2025-12-11 15:50:15 +0100

    Add cocktail data and return-cocktail-names function

--- src/cocktail_bar/core.clj
+++ src/cocktail_bar/core.clj
@@ -1,5 +1,22 @@
 (ns cocktail-bar.core)
-(defn hello
-  "Print a welcome message in the REPL."
+(def cocktails
+  [{:name "Mojito"
+    :type :alcoholic
+    :price 750
+    :ingredients [:rum :lime :sugar :mint :soda]}
+   {:name "Virgin Mojito"
+    :type :non-alcoholic
+    :price 550
+    :ingredients [:lime :sugar :mint :soda]}
+   {:name "Gin & Tonic"
+    :type :alcoholic
+    :price 650
+    :ingredients [:gin :tonic]}
+   {:name "Orange Juice"
+    :type :non-alcoholic
+    :price 400
+    :ingredients [:orange-juice]}])
+(defn return-cocktail-names
+  "Return a sequence of all cocktail names."
   []
-  (println "Welcom.idea/\nout/\ntarget/\n.cpcache/\n*.iml\ne to the Cocktail Bar REPL!"))
+  (map :name cocktails))
